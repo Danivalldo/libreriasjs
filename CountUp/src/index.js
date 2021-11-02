ed43c459f4e616b51fca5b84abf0e5a73488d732
Dani <dani@dimo.cat>
2021-11-02 12:08:58 +0100

    script for countUp

---
+++ CountUp/src/index.js
@@ -0,0 +1,33 @@
+import { CountUp } from "countup.js";
+import "./SCSS/index.scss";
+
+const duration = 6;
+
+const stars = new CountUp("count-up-container-top", 7000, {
+  prefix: "+ ",
+  suffix: " ⭐️",
+  duration: duration,
+  separator: ".",
+});
+
+const users = new CountUp("count-up-container-bottom", 15300, {
+  prefix: "+ ",
+  suffix: " 💻",
+  duration: duration,
+  separator: ".",
+});
+
+const endContainer = document.querySelector(".end-container");
+
+if (!stars.error && !users.error) {
+  stars.start();
+  window.setTimeout(() => {
+    users.start(() => {
+      window.setTimeout(() => {
+        endContainer.classList.add("active");
+      }, 1000);
+    });
+  }, (duration * 1000) / 3);
+} else {
+  console.error(stars.error);
+}
