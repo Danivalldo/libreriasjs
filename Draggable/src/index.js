a57f6af2927e7ec93c308b7f995f08894f7bce31
Dani <dani@dimo.cat>
2021-11-10 12:08:25 +0100

    improved script

--- Draggable/src/index.js
+++ Draggable/src/index.js
@@ -1,3 +1,31 @@
 import "./SCSS/index.scss";
 
+import { Droppable } from "@shopify/draggable";
+
+const launch = () => {
+  const containers = document.querySelectorAll("#chess-board");
+
+  if (containers.length === 0) {
+    return false;
+  }
+
+  const droppable = new Droppable(containers, {
+    draggable: ".Block--isDraggable",
+    dropzone: ".BlockWrapper--isDropzone",
+    mirror: {
+      constrainDimensions: true,
+    },
+  });
+
+  droppable.on("drag:start", (evt) => {
+    // console.log("start draggin");
+  });
+
+  droppable.on("droppable:dropped", (evt) => {
+    console.log(evt);
+  });
+};
+
+launch();
+
 console.log("Ready to go!");
