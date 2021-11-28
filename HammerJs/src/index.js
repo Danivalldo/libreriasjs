f6ed4ab627e59f3834db3502304ecdf9602683d2
Dani <dani@dimo.cat>
2021-11-28 01:15:56 +0100

    improved example

--- HammerJs/src/index.js
+++ HammerJs/src/index.js
@@ -1,4 +1,23 @@
 import "./SCSS/index.scss";
 import Hammer from "hammerjs";
 
-console.log("Ready to go!", Hammer);
+const activeCard = document.querySelector(".active-card");
+const cardPos = {
+  x: 0,
+  y: 0,
+};
+
+const pannableCard = new Hammer(activeCard);
+pannableCard.on("panstart", () => {
+  console.log("panstart");
+});
+pannableCard.on("pan", (ev) => {
+  console.log("panning");
+  activeCard.style.transform = `translate3d(${
+    cardPos.x + ev.deltaX
+  }px, 0px, 0px)`;
+});
+pannableCard.on("panend", (ev) => {
+  console.log("panend");
+  cardPos.x = cardPos.x + ev.deltaX;
+});
