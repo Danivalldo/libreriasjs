93dc75dad6ca3f21c630f13030d862b7dcf39810
Dani <dani@dimo.cat>
2021-11-28 20:30:48 +0100

    improved example

--- HammerJs/src/index.js
+++ HammerJs/src/index.js
@@ -1,23 +1,84 @@
 import "./SCSS/index.scss";
 import Hammer from "hammerjs";
 
+let techIndex = 1;
+
+const technologies = [
+  "react.svg",
+  "angularjs-plain.svg",
+  "vuejs.svg",
+  "css3-plain.svg",
+  "webpack.svg",
+  "nodejs.svg",
+  "sass.svg",
+  "jquery.svg",
+  "npm.svg",
+];
+
+const lerp = (x, y, a) => x * (1 - a) + y * a;
+const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
+const invlerp = (x, y, a) => clamp((a - x) / (y - x));
+const range = (x1, y1, x2, y2, a) => lerp(x2, y2, invlerp(x1, y1, a));
+
 const activeCard = document.querySelector(".active-card");
-const cardPos = {
-  x: 0,
-  y: 0,
-};
 
 const pannableCard = new Hammer(activeCard);
+
 pannableCard.on("panstart", () => {
   console.log("panstart");
+  activeCard.classList.add("dragging");
 });
+
 pannableCard.on("pan", (ev) => {
-  console.log("panning");
-  activeCard.style.transform = `translate3d(${
-    cardPos.x + ev.deltaX
-  }px, 0px, 0px)`;
+  const lateralDistance = window.innerWidth / 2;
+  const rotation = range(
+    -1 * lateralDistance,
+    lateralDistance,
+    -45,
+    45,
+    ev.deltaX
+  );
+  activeCard.style.transform = `translate3d(${ev.deltaX}px, 0px, 0px) rotate(${rotation}deg)`;
 });
+
+const createNewCard = () => {
+  if (techIndex > technologies.length - 1) {
+    techIndex = 0;
+  }
+  activeCard
+    .querySelector(".logo")
+    .setAttribute("src", `imgs/${technologies[techIndex]}`);
+  techIndex++;
+  activeCard.setAttribute("style", "");
+  activeCard.classList.remove("released-out", "accepted", "discarted");
+  activeCard.classList.add("recovering");
+  window.setTimeout(() => {
+    activeCard.classList.remove("recovering");
+  }, 150);
+};
+
 pannableCard.on("panend", (ev) => {
-  console.log("panend");
-  cardPos.x = cardPos.x + ev.deltaX;
+  const lateralDistance = window.innerWidth / 2;
+  activeCard.classList.remove("dragging");
+
+  if (ev.deltaX > lateralDistance * 0.5 || ev.deltaX < lateralDistance * -0.5) {
+    activeCard.classList.add("released-out");
+    activeCard.classList.add(ev.deltaX > 0 ? "accepted" : "discarted");
+    const rotation = range(
+      -1 * lateralDistance,
+      lateralDistance,
+      -45,
+      45,
+      ev.deltaX
+    );
+    activeCard.style.transform = `translate3d(${
+      ev.deltaX + 100 * (ev.deltaX > 0 ? 1 : -1)
+    }px, 0px, 0px) rotate(${rotation + 10 * (ev.deltaX > 0 ? 1 : -1)}deg)`;
+    window.setTimeout(() => {
+      createNewCard();
+    }, 500);
+    return;
+  }
+
+  activeCard.setAttribute("style", "");
 });
