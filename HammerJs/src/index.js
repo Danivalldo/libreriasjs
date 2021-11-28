d1842624accf5d63a9b16a81cd1e6dfb3cef0ccd
Dani <dani@dimo.cat>
2021-11-29 00:14:44 +0100

    change size of icons by panning movement

--- HammerJs/src/index.js
+++ HammerJs/src/index.js
@@ -21,6 +21,8 @@ const invlerp = (x, y, a) => clamp((a - x) / (y - x));
 const range = (x1, y1, x2, y2, a) => lerp(x2, y2, invlerp(x1, y1, a));
 
 const activeCard = document.querySelector(".active-card");
+const heartIcon = activeCard.querySelector(".heart-icon");
+const crossIcon = activeCard.querySelector(".cross-icon");
 
 const pannableCard = new Hammer(activeCard);
 
@@ -38,7 +40,23 @@ pannableCard.on("pan", (ev) => {
     45,
     ev.deltaX
   );
+  const sizeIconHeart = range(
+    -1 * lateralDistance,
+    lateralDistance,
+    1.6,
+    0,
+    ev.deltaX
+  );
+  const sizeIconCross = range(
+    -1 * lateralDistance,
+    lateralDistance,
+    0,
+    1.6,
+    ev.deltaX
+  );
   activeCard.style.transform = `translate3d(${ev.deltaX}px, 0px, 0px) rotate(${rotation}deg)`;
+  heartIcon.style.transform = `scale(${sizeIconHeart})`;
+  crossIcon.style.transform = `scale(${sizeIconCross})`;
 });
 
 const createNewCard = () => {
@@ -60,6 +78,9 @@ const createNewCard = () => {
 pannableCard.on("panend", (ev) => {
   const lateralDistance = window.innerWidth / 2;
   activeCard.classList.remove("dragging");
+
+  heartIcon.setAttribute("style", "");
+  crossIcon.setAttribute("style", "");
 
   if (ev.deltaX > lateralDistance * 0.5 || ev.deltaX < lateralDistance * -0.5) {
     activeCard.classList.add("released-out");
