d1781011884766866a27b61d685e03b0dbd89ade
Dani <dani@dimo.cat>
2021-12-07 13:13:53 +0100

    improved example

--- DayJs/src/index.js
+++ DayJs/src/index.js
@@ -1,4 +1,17 @@
 import "./SCSS/index.scss";
 import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
-console.log("Ready to go!", dayjs);
+dayjs.extend(relativeTime);
+
+const datetimeSelector = document.querySelector(".date-time__selector");
+
+const handleOnChangeSelector = (event) => {
+  const birthDay = dayjs(event.target.value);
+  if (!birthDay.isValid()) {
+    return;
+  }
+  console.log(birthDay.toNow(true));
+};
+
+datetimeSelector.addEventListener("change", handleOnChangeSelector);
