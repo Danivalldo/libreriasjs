644018564076cecc7f5633a2c38a6243fd9f89cf
Dani <dani@dimo.cat>
2021-12-09 23:43:15 +0100

    improved exercise

--- OpenLayers/src/index.js
+++ OpenLayers/src/index.js
@@ -1,7 +1,7 @@
 import "./SCSS/index.scss";
 import { Map, View } from "ol";
 import { Tile } from "ol/layer";
-import { OSM } from "ol/source";
+import { OSM, TileWMS } from "ol/source";
 import { fromLonLat } from "ol/proj";
 
 const map = new Map({
@@ -10,9 +10,28 @@ const map = new Map({
     new Tile({
       source: new OSM(),
     }),
+    new Tile({
+      source: new TileWMS({
+        projection: "EPSG:25831",
+        url: "https://geoserveis.icgc.cat/icc_mapesmultibase/utm/wms/service?",
+        params: {
+          LAYERS: "topo",
+        },
+      }),
+    }),
+    // https://www.icgc.cat/Administracio-i-empresa/Serveis/Geoinformacio-en-linia-Geoserveis/WMS-Vegetacio/WMS-Incendi-Ribera-d-Ebre-2019
+    new Tile({
+      source: new TileWMS({
+        projection: "EPSG:25831",
+        url: "https://geoserveis.icgc.cat/servei/catalunya/incendis/wms/service?",
+        params: {
+          LAYERS: "20190701-RiberaEbre-orto15cm-rgb",
+        },
+      }),
+    }),
   ],
   view: new View({
-    center: fromLonLat([37.41, 8.82]),
-    zoom: 4,
+    center: fromLonLat([2.174288, 41.403287]),
+    zoom: 20,
   }),
 });
