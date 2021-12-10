0c1cf8da21b0150b51188e76ee065c487942af18
Dani <dani@dimo.cat>
2021-12-10 18:31:18 +0100

    exercie finished

--- OpenLayers/src/index.js
+++ OpenLayers/src/index.js
@@ -3,35 +3,88 @@ import { Map, View } from "ol";
 import { Tile } from "ol/layer";
 import { OSM, TileWMS } from "ol/source";
 import { fromLonLat } from "ol/proj";
+import GUI from "lil-gui";
+
+const lilGuiBox = new GUI();
+
+const guiActions = {
+  showTileLayerOSM: true,
+  showTileLayerTopoCat: true,
+  showTileLayerSummer2019Cat: true,
+  showTileLayerFire2019Cat: true,
+  fireLayerOpacity: 1,
+};
+
+const tileLayerOSM = new Tile({
+  source: new OSM(),
+});
+
+const tileLayerTopoCat = new Tile({
+  source: new TileWMS({
+    projection: "EPSG:25831",
+    url: "https://geoserveis.icgc.cat/icc_mapesmultibase/utm/wms/service?",
+    params: {
+      LAYERS: "topo",
+    },
+  }),
+});
+
+const tileLayerSummer2019Cat = new Tile({
+  source: new TileWMS({
+    projection: "EPSG:25831",
+    url: "https://geoserveis.icgc.cat/icgc_sentinel2/wms/service?",
+    params: {
+      LAYERS: "sen2rgb_mosaic_estiu_2019",
+    },
+  }),
+});
+
+const tileLayerFire2019Cat = new Tile({
+  source: new TileWMS({
+    projection: "EPSG:25831",
+    url: "https://geoserveis.icgc.cat/servei/catalunya/incendis/wms/service?",
+    params: {
+      LAYERS: "20190701-RiberaEbre-orto15cm-rgb",
+    },
+  }),
+});
 
 const map = new Map({
   target: "map",
   layers: [
-    new Tile({
-      source: new OSM(),
-    }),
-    new Tile({
-      source: new TileWMS({
-        projection: "EPSG:25831",
-        url: "https://geoserveis.icgc.cat/icc_mapesmultibase/utm/wms/service?",
-        params: {
-          LAYERS: "topo",
-        },
-      }),
-    }),
-    // https://www.icgc.cat/Administracio-i-empresa/Serveis/Geoinformacio-en-linia-Geoserveis/WMS-Vegetacio/WMS-Incendi-Ribera-d-Ebre-2019
-    new Tile({
-      source: new TileWMS({
-        projection: "EPSG:25831",
-        url: "https://geoserveis.icgc.cat/servei/catalunya/incendis/wms/service?",
-        params: {
-          LAYERS: "20190701-RiberaEbre-orto15cm-rgb",
-        },
-      }),
-    }),
+    tileLayerOSM,
+    tileLayerTopoCat,
+    tileLayerSummer2019Cat,
+    tileLayerFire2019Cat,
   ],
   view: new View({
-    center: fromLonLat([2.174288, 41.403287]),
-    zoom: 20,
+    center: fromLonLat([0.600712, 41.280345]),
+    zoom: 12,
   }),
 });
+
+lilGuiBox
+  .add(guiActions, "showTileLayerFire2019Cat")
+  .name("Capa incendio 2019")
+  .onChange((visible) => {
+    tileLayerFire2019Cat.setVisible(visible);
+  });
+lilGuiBox
+  .add(guiActions, "showTileLayerSummer2019Cat")
+  .name("Capa vegetación 2019")
+  .onChange((visible) => {
+    tileLayerSummer2019Cat.setVisible(visible);
+  });
+lilGuiBox
+  .add(guiActions, "showTileLayerTopoCat")
+  .name("Capa toponímia Catalunya")
+  .onChange((visible) => {
+    tileLayerTopoCat.setVisible(visible);
+  });
+
+lilGuiBox
+  .add(guiActions, "showTileLayerOSM")
+  .name("Capa Open Street Map")
+  .onChange((visible) => {
+    tileLayerOSM.setVisible(visible);
+  });
