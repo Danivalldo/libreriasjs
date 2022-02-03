bc5c21d15147cf98458d3d6644581055da09ab99
Dani <dani@dimo.cat>
2022-02-03 14:37:25 +0100

    prepared exercise for miniMasonry

---
+++ MiniMasonry/src/index.js
@@ -0,0 +1,8 @@
+import MiniMasonry from "minimasonry";
+import "./SCSS/index.scss";
+
+const miniMasonry = new MiniMasonry({
+  container: ".container",
+});
+
+console.log("Ready to go!", MiniMasonry);
