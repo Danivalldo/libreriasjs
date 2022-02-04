e1efac03e2e504812419b8c1ca2f495aed5f9283
Dani <dani@dimo.cat>
2022-02-04 13:02:37 +0100

    improved logic to relaunch layout after each image loaded

--- MiniMasonry/src/index.js
+++ MiniMasonry/src/index.js
@@ -1,8 +1,16 @@
 import MiniMasonry from "minimasonry";
 import "./SCSS/index.scss";
 
+const container = document.querySelector(".container");
+
 const miniMasonry = new MiniMasonry({
-  container: ".container",
+  container,
+});
+
+container.querySelectorAll("img").forEach((img) => {
+  img.addEventListener("load", () => {
+    miniMasonry.layout();
+  });
 });
 
 console.log("Ready to go!", MiniMasonry);
