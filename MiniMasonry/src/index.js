3083857a071857b0b0b4cb32dbebebc4f2387ba0
Dani <dani@dimo.cat>
2022-02-10 12:39:15 +0100

    prepared load on scroll

--- MiniMasonry/src/index.js
+++ MiniMasonry/src/index.js
@@ -2,15 +2,39 @@ import MiniMasonry from "minimasonry";
 import "./SCSS/index.scss";
 
 const container = document.querySelector(".container");
+let isLoading = false;
 
 const miniMasonry = new MiniMasonry({
   container,
 });
 
+const handleOnLoadImage = () => {
+  miniMasonry.layout();
+};
+
+const addNewImages = () => {
+  console.log("add new images");
+  window.setTimeout(() => {
+    isLoading = false;
+  }, 200);
+};
+
+const isScrollNearBottom = () => {
+  const scrollHeight = document.documentElement.scrollHeight;
+  const scrollTop = document.documentElement.scrollTop;
+  const clientHeight = document.documentElement.clientHeight;
+  return scrollHeight - scrollTop <= clientHeight + 100;
+};
+
+window.addEventListener("scroll", () => {
+  if (isScrollNearBottom() && !isLoading) {
+    isLoading = true;
+    addNewImages();
+  }
+});
+
 container.querySelectorAll("img").forEach((img) => {
-  img.addEventListener("load", () => {
-    miniMasonry.layout();
-  });
+  img.addEventListener("load", handleOnLoadImage);
 });
 
 console.log("Ready to go!", MiniMasonry);
