78979f137b8de40e8274a03aa1a0fd5d99ed7ad7
Dani <dani@dimo.cat>
2022-03-03 11:53:39 +0100

    prepared on submit callback

--- JsPDF/src/index.js
+++ JsPDF/src/index.js
@@ -1,2 +1,24 @@
 import { jsPDF } from "jspdf";
 import "./SCSS/index.scss";
+
+const formCharacterProfile = document.querySelector("#form-character-profile");
+
+formCharacterProfile.addEventListener("submit", (e) => {
+  e.preventDefault();
+  try {
+    const characterProperties = Array.from(
+      e.target.querySelectorAll("[name]")
+    ).map((field) => {
+      if (!field.value) {
+        throw new Error(`El campo ${field.getAttribute("name")} está vacio!`);
+      }
+      return {
+        attribute: field.getAttribute("name"),
+        value: field.value,
+      };
+    });
+    console.log(characterProperties);
+  } catch (err) {
+    console.log(err);
+  }
+});
