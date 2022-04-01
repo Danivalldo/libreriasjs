20024975c5ae4092178c5dd13c836577dff35e86
Dani <dani@dimo.cat>
2022-04-01 16:21:16 +0200

    added data

---
+++ Tagify/src/movies.js
@@ -0,0 +1,42 @@
+export const movies = [
+  {
+    title: "Lord of the rings",
+    gif: "https://giphy.com/embed/BZMggpshzrPvbfQHIF/",
+    suggestedTags: ["No puedes pasar!", "Mi tesoro...", "Sr Frodo"],
+  },
+  {
+    title: "Star Wars",
+    gif: "https://giphy.com/embed/3ornjSL2sBcPflIDiU",
+    suggestedTags: ["Sables láser", "Ciencia ficción", "Yo soy tu padre"],
+  },
+  {
+    title: "Jurassic Park",
+    gif: "https://giphy.com/embed/hu1TLAaubiak9S4GT6",
+    suggestedTags: ["Live finds a way", "Velociraptors"],
+  },
+  {
+    title: "Little Miss Sunshine",
+    gif: "https://giphy.com/embed/ThMIpSgPVFS12",
+    suggestedTags: ["Roadmovie", "Steve Carell"],
+  },
+  {
+    title: "Inception",
+    gif: "https://giphy.com/embed/mSf4QMJjbvJYI",
+    suggestedTags: ["Leo DiCaprio", "Sueños", "Espectacular"],
+  },
+  {
+    title: "Batman",
+    gif: "https://giphy.com/embed/EMpPEre2PqFy8GkOZE",
+    suggestedTags: ["I am Batman", "2022"],
+  },
+  {
+    title: "The Shinning",
+    gif: "https://giphy.com/embed/4m7zfhchURMRO",
+    suggestedTags: ["Nicholson", "Ven a jugar con nosotras Danny", "Redrum"],
+  },
+  {
+    title: "Nightmare before christmas",
+    gif: "https://giphy.com/embed/yidUzHwwoFalzhyKXu",
+    suggestedTags: ["Jack Skellington", "Stop Motion", "Halloween"],
+  },
+];
