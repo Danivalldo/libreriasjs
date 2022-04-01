1ee0dd400802907ae780eebbdd861664237738df
Dani <dani@dimo.cat>
2022-04-01 16:21:34 +0200

    improved script

--- Tagify/src/index.js
+++ Tagify/src/index.js
@@ -1,4 +1,62 @@
-import {} from "@yaireo/tagify";
+import Tagify from "@yaireo/tagify";
+import { movies } from "./movies";
 import "./SCSS/index.scss";
 
-console.log("Ready to go!");
+const currentMovieContainer = document.querySelector(".current-movie");
+const gifIFrameContainer = currentMovieContainer.querySelector(
+  ".gif-iframe-container"
+);
+const movieTitleContainer = currentMovieContainer.querySelector(".movie-title");
+const moviesListContainer = document.querySelector(".movies-list-container ul");
+
+const btnSave = document.querySelector(".btn-save");
+
+const tagify = new Tagify(document.querySelector(".input-tagify"));
+let i = 0;
+
+const buildMovieTemplate = (movie, tags) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <div>
+      <h3 class="text-center">${movie.title}</h3>
+      <input type="text" value="${tags
+        .map((tag) => tag.value)
+        .join(",")}" disabled />
+    </div>
+  `;
+  return li;
+};
+
+const saveCurrentMovie = (newMovieData) => {
+  let tags = tagify.getInputValue();
+  const liElement = buildMovieTemplate(
+    newMovieData,
+    tags ? JSON.parse(tags) : []
+  );
+  moviesListContainer.appendChild(liElement);
+  new Tagify(liElement.querySelector("input"), {});
+  tagify.removeAllTags();
+  console.log(tagify);
+  tagify.DOM.input.focus();
+};
+
+const loadMovie = (newMovieData) => {
+  tagify.addTags(newMovieData.suggestedTags);
+  gifIFrameContainer.setAttribute("src", newMovieData.gif);
+  movieTitleContainer.innerHTML = newMovieData.title;
+};
+
+btnSave.addEventListener("click", () => {
+  saveCurrentMovie(movies[i]);
+  if (i >= movies.length - 1) {
+    // currentMovieContainer.innerHTML = "<h2 class='text-center'>FIN</h2>";
+    movieTitleContainer.innerHTML = "";
+    gifIFrameContainer.parentNode.removeChild(gifIFrameContainer);
+    btnSave.setAttribute("disabled", "");
+    tagify.setDisabled(true);
+    return;
+  }
+  loadMovie(movies[++i]);
+});
+
+loadMovie(movies[i]);
