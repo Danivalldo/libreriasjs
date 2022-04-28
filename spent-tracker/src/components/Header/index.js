5dcd1e1e5d99a7bd85403a2afa249935b12c63f1
Dani <dani@dimo.cat>
2022-04-28 23:53:29 +0200

    improved component

--- spent-tracker/src/components/Header/index.js
+++ spent-tracker/src/components/Header/index.js
@@ -1,9 +1,14 @@
+import Illustration from "../Illustration";
 import styles from "./header.module.css";
 
 const Header = () => {
   return (
     <div className={styles["header-container"]}>
-      <h1>Header</h1>
+      <div>
+        <h1 className={styles.title}>Expense Tracker</h1>
+      </div>
+      <Illustration />
+      <div className={styles.wave}></div>
     </div>
   );
 };
