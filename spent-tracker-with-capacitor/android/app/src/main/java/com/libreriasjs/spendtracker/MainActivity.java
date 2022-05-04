f49baa3f55484c17afe683a3c381a1cfc11f8922
Dani <dani@dimo.cat>
2022-05-04 21:12:50 +0200

    native files of each platform

---
+++ spent-tracker-with-capacitor/android/app/src/main/java/com/libreriasjs/spendtracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.libreriasjs.spendtracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ spent-tracker-with-capacitor/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.libreriasjs.spendtracker',
+  appName: 'spend-tracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
