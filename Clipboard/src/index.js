480b68e778a6c0c83fedafdda944e7f5fdb466c8
Dani <dani@dimo.cat>
2022-05-05 12:50:59 +0200

    improved script

--- Clipboard/src/index.js
+++ Clipboard/src/index.js
@@ -1,4 +1,9 @@
 import ClipboardJS from "clipboard";
 import "./SCSS/index.scss";
 
+const textblock = document.querySelector(".chat-box__container textarea");
 const clipboard = new ClipboardJS(".copy-btn");
+
+clipboard.on("success", (copiedContent) => {
+  textblock.value = copiedContent.text;
+});
