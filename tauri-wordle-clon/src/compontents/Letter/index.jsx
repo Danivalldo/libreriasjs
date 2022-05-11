a509d742ac73d0f7404ff9093eb75edbc16e5e41
Dani <dani@dimo.cat>
2022-05-11 20:19:23 +0200

    improved component

--- tauri-wordle-clon/src/compontents/Letter/index.jsx
+++ tauri-wordle-clon/src/compontents/Letter/index.jsx
@@ -2,8 +2,9 @@ import styles from './letter.module.css';
 
 const Letter = ({letter, result}) => {
   return (
-    <div className={`${styles.container} ${typeof result === 'number'?styles[`result-${result}`]:''}`}>
-      {letter}
+    <div className={`${styles.container}`}>
+      <span className={styles['letter-wrapper']}>{letter}</span>
+      <span className={`${typeof result === 'number'?styles.active:''} ${styles['bg-letter']} ${typeof result === 'number'?styles[`result-${result}`]:''}`}></span>
     </div>
   );
 };
