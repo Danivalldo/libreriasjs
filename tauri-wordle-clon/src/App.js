4c56ca13f22716e147e2099cc990b5d882800343
Dani <dani@dimo.cat>
2022-05-11 20:19:14 +0200

    improved game

--- tauri-wordle-clon/src/App.js
+++ tauri-wordle-clon/src/App.js
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import wordleGuess from "./services/wordGuess";
 import Word from "./compontents/Word";
 import "./App.css";
 
-function App() {
-  const sizeWord = 5;
+const sizeWord = 5;
+const maxAttempts = 5;
 
+function App() {
   const fillWithNull = (max) => {
     const arr = [];
     for (let i = 0; i < max; i++) {
@@ -13,6 +14,8 @@ function App() {
     }
     return arr;
   };
+
+  const [gameOver, setGameOver] = useState(false);
 
   const [attempts, setAttempts] = useState([
     {
@@ -40,54 +43,75 @@ function App() {
     return guessed;
   };
 
-  const handleOnKeyUp = (e) => {
-    if (e.key === "Backspace") {
-      return setAttempts((prevAttempts) => {
-        const nextGuessed = removeLastLetter(
-          prevAttempts[prevAttempts.length - 1].guessed
-        );
-        prevAttempts[prevAttempts.length - 1] = {
-          guessed: nextGuessed,
-        };
-        return [...prevAttempts];
-      });
-    }
-    if ((e.which >= 65 && e.which <= 90) || e.which === 186) {
-      return setAttempts((prevAttempts) => {
-        const nextGuessed = fillNextLetter(
-          prevAttempts[prevAttempts.length - 1].guessed,
-          e.key
-        );
-
-        prevAttempts[prevAttempts.length - 1] = {
-          guessed: nextGuessed,
-        };
-        return [...prevAttempts];
-      });
-    }
-    if (e.key === "Enter") {
-      return setAttempts((prevAttempts) => {
-        if (
-          prevAttempts[prevAttempts.length - 1].guessed[
-            prevAttempts[prevAttempts.length - 1].guessed.length - 1
-          ] === null
-        ) {
+  const handleOnKeyUp = useCallback(
+    (e) => {
+      if (gameOver) {
+        return;
+      }
+      if (e.key === "Backspace") {
+        return setAttempts((prevAttempts) => {
+          const nextGuessed = removeLastLetter(
+            prevAttempts[prevAttempts.length - 1].guessed
+          );
+          prevAttempts[prevAttempts.length - 1] = {
+            guessed: nextGuessed,
+          };
           return [...prevAttempts];
-        }
-        const updatedAttemps = [
-          ...prevAttempts,
-          {
+        });
+      }
+      if ((e.which >= 65 && e.which <= 90) || e.which === 186) {
+        return setAttempts((prevAttempts) => {
+          const nextGuessed = fillNextLetter(
+            prevAttempts[prevAttempts.length - 1].guessed,
+            e.key
+          );
+
+          prevAttempts[prevAttempts.length - 1] = {
+            guessed: nextGuessed,
+          };
+          return [...prevAttempts];
+        });
+      }
+      if (e.key === "Enter") {
+        return setAttempts((prevAttempts) => {
+          if (
+            prevAttempts[prevAttempts.length - 1].guessed[
+              prevAttempts[prevAttempts.length - 1].guessed.length - 1
+            ] === null
+          ) {
+            return [...prevAttempts];
+          }
+
+          const updatedAttemps = [...prevAttempts];
+          const resultGuessed = wordleGuess(
+            prevAttempts[prevAttempts.length - 1].guessed.join(""),
+            "MUNDO"
+          );
+          const idResult = resultGuessed.join("");
+          updatedAttemps[updatedAttemps.length - 1].result = resultGuessed;
+          if (updatedAttemps.length >= maxAttempts || idResult === "22222") {
+            setGameOver({
+              won: idResult === "22222",
+            });
+            return updatedAttemps;
+          }
+          updatedAttemps.push({
             guessed: fillWithNull(sizeWord),
-          },
-        ];
-        const resultGuessed = wordleGuess(
-          prevAttempts[prevAttempts.length - 1].guessed.join(""),
-          "MUNDO"
-        );
-        updatedAttemps[updatedAttemps.length - 2].result = resultGuessed;
-        return updatedAttemps;
-      });
-    }
+          });
+          return updatedAttemps;
+        });
+      }
+    },
+    [gameOver]
+  );
+
+  const handleRestart = () => {
+    setAttempts([
+      {
+        guessed: fillWithNull(sizeWord),
+      },
+    ]);
+    setGameOver(false);
   };
 
   useEffect(() => {
@@ -95,13 +119,19 @@ function App() {
     return () => {
       window.removeEventListener("keyup", handleOnKeyUp);
     };
-  }, []);
+  }, [gameOver]);
 
   return (
     <div className="App">
       {attempts.map((attempt, i) => {
         return <Word key={i} word={attempt.guessed} result={attempt.result} />;
       })}
+      {gameOver && (
+        <>
+          <p>You {gameOver.won ? "WON!" : "LOSE"}</p>
+          <button onClick={handleRestart}>Restart</button>
+        </>
+      )}
     </div>
   );
 }
