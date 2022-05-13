f217c9b76ff22019c4e563ddd5c345f43da5407f
Dani <dani@dimo.cat>
2022-05-13 11:52:57 +0200

    created modal

---
+++ tauri-wordle-clon/src/compontents/Modal/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./modal.module.css";
+
+const Modal = ({ active, children }) => {
+  return (
+    <div className={`${styles.container} ${active && styles.active}`}>
+      <div className={styles["children-container"]}>{children}</div>
+    </div>
+  );
+};
+
+export default Modal;
