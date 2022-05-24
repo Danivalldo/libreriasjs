915ca214e239fea6184ad256471dcdfcd2626904
Dani <dani@dimo.cat>
2022-05-24 12:21:35 +0200

    improved controller to handle onSubmit, onChange some fields and show errors

--- Yup/src/scripts/RecipeManagement.js
+++ Yup/src/scripts/RecipeManagement.js
@@ -1,7 +1,7 @@
 import templateColIngredient from "./template-col-ingredient";
 
 class RecipeManagement {
-  constructor() {
+  constructor(cbOnSubmit) {
     this.recipe = {
       title: undefined,
       description: undefined,
@@ -10,16 +10,48 @@ class RecipeManagement {
       numDiners: undefined,
       ingredients: [],
       instructions: undefined,
+      acceptance: false,
     };
+    this.cbOnSubmit = cbOnSubmit;
     this.form = document.querySelector(".recipe-form");
+    this.errorsContainer = this.form.querySelector(".errors-list");
     this.addListeners();
   }
   addListeners() {
-    this.form.addEventListener("click", (e) => {
-      if (e.target.classList.contains("add-ingredients-btn")) {
-        return this.handleOnClickIngredientBtn(e.target);
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (typeof this.cbOnSubmit === "function") {
+        this.updateRecipe();
+        this.cbOnSubmit(this.getRecipe());
       }
     });
+    this.form
+      .querySelector(".field-ingredients")
+      .addEventListener("click", (e) => {
+        if (e.target.classList.contains("add-ingredients-btn")) {
+          return this.handleOnClickIngredientBtn(e.target);
+        }
+      });
+    this.form
+      .querySelector('input[type="checkbox"]')
+      .addEventListener("change", (e) => {
+        this.recipe.acceptance = e.target.checked;
+      });
+    this.form
+      .querySelector('input[type="file"]')
+      .addEventListener("change", (e) => {
+        if (e.target.files.length <= 0) {
+          return;
+        }
+        const file = e.target.files[0];
+        console.log(file);
+        e.currentTarget.closest(".file").querySelector(".file-name").innerHTML =
+          file.name;
+        this.recipe.image = {
+          size: file.size,
+          type: file.type,
+        };
+      });
   }
   handleOnClickIngredientBtn(ingredientBtn) {
     const colIngredient = ingredientBtn.parentElement.parentElement;
@@ -60,6 +92,28 @@ class RecipeManagement {
     const element = template.children[0].children[1].children[0];
     colIngredient.parentElement.appendChild(element);
     console.log(this.recipe.ingredients);
+  }
+  updateRecipe() {
+    this.errorsContainer.classList.add("is-hidden");
+    const fields = this.form.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      this.recipe[field.getAttribute("name")] = field.value;
+    });
+  }
+  applyErrors(errors) {
+    this.errorsContainer.classList.remove("is-hidden");
+    this.errorsContainer.innerHTML = "";
+    const ul = document.createElement("ul");
+    Object.keys(errors).forEach((errorKey) => {
+      const message = errors[errorKey];
+      const li = document.createElement("li");
+      li.innerHTML = message;
+      ul.appendChild(li);
+    });
+    this.errorsContainer.appendChild(ul);
+  }
+  getRecipe() {
+    return this.recipe;
   }
 }
 
