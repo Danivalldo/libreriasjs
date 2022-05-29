b0538e0b80e0577496b8a96284e1e71113489f0c
Dani <dani@dimo.cat>
2022-05-29 13:34:21 +0200

    improved servicee

--- library-to-component/src/services/ScratchcardService.js
+++ library-to-component/src/services/ScratchcardService.js
@@ -1,4 +1,11 @@
-import { Application, Graphics, RenderTexture, Sprite } from "pixi.js";
+import {
+  Application,
+  Graphics,
+  RenderTexture,
+  Sprite,
+  filters,
+  Texture,
+} from "pixi.js";
 
 class ScratchCardService {
   constructor() {
@@ -8,8 +15,11 @@ class ScratchCardService {
     this.brush = undefined;
     this.dragging = false;
     this.renderTexture = undefined;
+    this.background = undefined;
+    this.imageToReveal = undefined;
+    this.renderTextureSprite = undefined;
   }
-  launch(container) {
+  launch(container, { frontImage, backImage }) {
     const containerSize = container.getBoundingClientRect();
     this.app = new Application({
       width: containerSize.width,
@@ -19,42 +29,48 @@ class ScratchCardService {
     this.brush = new Graphics();
     this.brush.beginFill(0xffffff);
     this.brush.drawCircle(0, 0, 50);
+    this.brush.filters = [new filters.BlurFilter(5)];
     this.brush.endFill();
     this.container = container;
     this.container.appendChild(this.app.view);
-    this.loadResources();
     this.renderTexture = RenderTexture.create({
       width: this.app.screen.width,
       height: this.app.screen.height,
     });
+    this.background = new Sprite(Texture.from(backImage));
+    this.stage.addChild(this.background);
+    this.imageToReveal = new Sprite(Texture.from(frontImage));
+    this.stage.addChild(this.imageToReveal);
+    this.renderTextureSprite = new Sprite(this.renderTexture);
+    this.stage.addChild(this.renderTextureSprite);
+
+    this.background.width = this.app.screen.width;
+    this.background.height = this.app.screen.height;
+
+    this.imageToReveal.width = this.app.screen.width;
+    this.imageToReveal.height = this.app.screen.height;
+    this.imageToReveal.mask = this.renderTextureSprite;
+
     this.stage.interactive = true;
     this.stage.on("pointerdown", this.pointerDown.bind(this));
     this.stage.on("pointerup", this.pointerUp.bind(this));
     this.stage.on("pointermove", this.pointerMove.bind(this));
     this.dragging = false;
-    this.app.loader.load(this.setup.bind(this));
+    this.loadResources(frontImage, backImage);
   }
-  loadResources() {
-    this.app.loader.add("t1", "textures/bg_grass.jpeg");
-    this.app.loader.add("t2", "textures/bg_rotate.jpeg");
+  loadResources(frontImage, backImage) {
+    if (!this.app.loader.resources.t1 && !this.app.loader.resources.t1) {
+      this.app.loader.add("t1", frontImage);
+      this.app.loader.add("t2", backImage);
+      this.app.loader.load(this.setup.bind(this));
+      return;
+    }
   }
-  updateImages(frontImage, backImage) {}
-  setup(loader, resources) {
-    const background = new Sprite(resources.t1.texture);
-
-    this.stage.addChild(background);
-    background.width = this.app.screen.width;
-    background.height = this.app.screen.height;
-
-    const imageToReveal = new Sprite(resources.t2.texture);
-    this.stage.addChild(imageToReveal);
-    imageToReveal.width = this.app.screen.width;
-    imageToReveal.height = this.app.screen.height;
-
-    const renderTextureSprite = new Sprite(this.renderTexture);
-    this.stage.addChild(renderTextureSprite);
-    imageToReveal.mask = renderTextureSprite;
+  updateImages(frontImage, backImage) {
+    this.background.texture = Texture.from(backImage);
+    this.imageToReveal.texture = Texture.from(frontImage);
   }
+  setup(loader, resources) {}
   pointerMove(event) {
     if (!event || !this.renderTexture) {
       return;
