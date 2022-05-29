ab41b6275adb8aac29ba3afa5d6c4d3d6e2f27df
Dani <dani@dimo.cat>
2022-05-29 18:06:27 +0200

    formatted and updated component

--- library-to-component/src/components/ScratchCard.jsx
+++ library-to-component/src/components/ScratchCard.jsx
@@ -1,53 +1,61 @@
-import {useEffect, useMemo, useRef} from 'react';
+import { useEffect, useMemo, useRef } from "react";
 import ScratchCardService from "../services/ScratchcardService";
 
 const ScratchCard = ({
-  frontImage, 
+  frontImage,
   backImage,
+  radius,
   onScratchStart,
   onScratchEnd,
-  onScratching
+  onScratching,
 }) => {
   const containerRef = useRef(null);
-  
-  const scratchCardSrv = useMemo(()=>{
+
+  const scratchCardSrv = useMemo(() => {
     return new ScratchCardService();
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     scratchCardSrv.launch(containerRef.current, {
       frontImage,
       backImage,
     });
   }, []);
 
-  useEffect(()=>{
-    if(typeof onScratchStart !== 'function'){
+  useEffect(() => {
+    scratchCardSrv.setBrush(radius);
+  }, [radius]);
+
+  useEffect(() => {
+    if (typeof onScratchStart !== "function") {
       return;
     }
-    scratchCardSrv.on('scratchstart', onScratchStart);
+    scratchCardSrv.on("scratchstart", onScratchStart);
   }, [onScratchStart]);
 
-  useEffect(()=>{
-    if(typeof onScratchEnd !== 'function'){
+  useEffect(() => {
+    if (typeof onScratchEnd !== "function") {
       return;
     }
-    scratchCardSrv.on('scratchend', onScratchEnd);
+    scratchCardSrv.on("scratchend", onScratchEnd);
   }, [onScratchEnd]);
 
-  useEffect(()=>{
-    if(typeof onScratching !== 'function'){
+  useEffect(() => {
+    if (typeof onScratching !== "function") {
       return;
     }
-    scratchCardSrv.on('scratching', onScratching);
+    scratchCardSrv.on("scratching", onScratching);
   }, [onScratching]);
 
-  useEffect(()=>{
-    scratchCardSrv.updateImages(frontImage, backImage)
+  useEffect(() => {
+    scratchCardSrv.updateImages(frontImage, backImage);
   }, [frontImage, backImage]);
 
   return (
-    <div ref={containerRef} style={{height: 400, border: 'solid 1px red'}}></div>
+    <div
+      ref={containerRef}
+      style={{ height: 400, border: "solid 1px red" }}
+    ></div>
   );
 };
 
