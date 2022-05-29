52aac4fe5f160e37d5ec364e92e1ae2d8302f41d
Dani <dani@dimo.cat>
2022-05-29 13:49:16 +0200

    catch custom handlers

--- library-to-component/src/components/ScratchCard.jsx
+++ library-to-component/src/components/ScratchCard.jsx
@@ -4,6 +4,9 @@ import ScratchCardService from "../services/ScratchcardService";
 const ScratchCard = ({
   frontImage, 
   backImage,
+  onScratchStart,
+  onScratchEnd,
+  onScratching
 }) => {
   const containerRef = useRef(null);
   
@@ -17,6 +20,27 @@ const ScratchCard = ({
       backImage,
     });
   }, []);
+
+  useEffect(()=>{
+    if(typeof onScratchStart !== 'function'){
+      return;
+    }
+    scratchCardSrv.on('scratchstart', onScratchStart);
+  }, [onScratchStart]);
+
+  useEffect(()=>{
+    if(typeof onScratchEnd !== 'function'){
+      return;
+    }
+    scratchCardSrv.on('scratchend', onScratchEnd);
+  }, [onScratchEnd]);
+
+  useEffect(()=>{
+    if(typeof onScratching !== 'function'){
+      return;
+    }
+    scratchCardSrv.on('scratching', onScratching);
+  }, [onScratching]);
 
   useEffect(()=>{
     scratchCardSrv.updateImages(frontImage, backImage)
