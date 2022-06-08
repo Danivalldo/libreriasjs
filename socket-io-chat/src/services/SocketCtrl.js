f7616054f766a982cc83564f1015947a034f6fec
Dani <dani@dimo.cat>
2022-06-08 16:23:49 +0200

    created service to manage sockets

---
+++ socket-io-chat/src/services/SocketCtrl.js
@@ -0,0 +1,48 @@
+import { io } from "socket.io-client";
+
+class SocketCtrl {
+  constructor() {
+    this.socket = undefined;
+  }
+  connect(onConnected) {
+    if (this.socket) {
+      return;
+    }
+    this.socket = io("http://localhost:9000", {
+      query: {
+        type: "",
+      },
+    });
+    this.socket.on("connect", () => {
+      if (typeof onConnected === "function") {
+        onConnected(this.socket);
+      }
+    });
+  }
+
+  emit(key, data) {
+    if (!this.socket) {
+      return;
+    }
+    this.socket.emit(key, data);
+  }
+
+  on(key, cb) {
+    if (!this.socket) {
+      return;
+    }
+    this.socket.on(key, cb);
+  }
+
+  destroy() {
+    if (!this.socket) {
+      return;
+    }
+    if (this.socket.connected) {
+      this.socket.disconnect();
+    }
+    this.socket = undefined;
+  }
+}
+
+export default SocketCtrl;
