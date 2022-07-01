a90c216af6fef9baf20c6ee2ba29c33ac4623d14
Dani <dani@dimo.cat>
2022-07-01 17:22:02 +0200

    prepared script

--- Firebase/src/index.js
+++ Firebase/src/index.js
@@ -1,3 +1,19 @@
+import FirebaseCtrl from "./scripts/FirebaseCtrl";
 import "./SCSS/index.scss";
 
-console.log("Ready to go!");
+const firebaseCtrl = new FirebaseCtrl();
+
+window.addEventListener("load", () => {
+  firebaseCtrl.initApp();
+  document
+    .querySelector("#anonymous-log-in-btn")
+    .addEventListener("click", () => {
+      firebaseCtrl.logInAnonymously();
+    });
+  document.querySelector("#google-log-in-btn").addEventListener("click", () => {
+    firebaseCtrl.logInWithGoogle();
+  });
+  document.querySelector("#log-out").addEventListener("click", () => {
+    firebaseCtrl.logOut();
+  });
+});
