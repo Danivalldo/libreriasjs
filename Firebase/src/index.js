da15c4a1ff38031dfbe1d41e0a34c8c857048c5a
Dani Valldosera <dani@dimo.cat>
2022-07-01 22:32:45 +0200

    improved script

--- Firebase/src/index.js
+++ Firebase/src/index.js
@@ -5,15 +5,30 @@ const firebaseCtrl = new FirebaseCtrl();
 
 window.addEventListener("load", () => {
   firebaseCtrl.initApp();
+  firebaseCtrl.on("userloginstarted", () => {
+    document.body.classList.add("logging");
+  });
+  firebaseCtrl.on("userloginended", () => {
+    document.body.classList.remove("logging");
+  });
+  firebaseCtrl.on("userauthchanged", (user) => {
+    if (user) {
+      document.body.classList.add("logged-in");
+      console.log(user);
+      return;
+    }
+    document.body.classList.remove("logged-in");
+    document.body.classList.remove("logging");
+  });
   document
-    .querySelector("#anonymous-log-in-btn")
+    .querySelector(".anonymous-log-in-btn")
     .addEventListener("click", () => {
       firebaseCtrl.logInAnonymously();
     });
-  document.querySelector("#google-log-in-btn").addEventListener("click", () => {
+  document.querySelector(".google-log-in-btn").addEventListener("click", () => {
     firebaseCtrl.logInWithGoogle();
   });
-  document.querySelector("#log-out").addEventListener("click", () => {
+  document.querySelector(".log-out").addEventListener("click", () => {
     firebaseCtrl.logOut();
   });
 });
