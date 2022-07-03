d9ce1b6f979017cc7bd2d2052ea0a312a1323c42
Dani <dani@dimo.cat>
2022-07-03 17:48:53 +0200

    improved uiController

--- Firebase/src/scripts/UiCtrl.js
+++ Firebase/src/scripts/UiCtrl.js
@@ -1,3 +1,5 @@
+import templateNovelPortion from "./templateNovelPortion";
+
 class UiCtrl {
   constructor() {
     this.body = undefined;
@@ -8,6 +10,14 @@ class UiCtrl {
     this.anonymousLogInBtn = document.querySelector(".anonymous-log-in-btn");
     this.googleLogInBtn = document.querySelector(".google-log-in-btn");
     this.logOutBtn = document.querySelector(".log-out-btn");
+    this.novelContainer = document.querySelector(".novel-container");
+    this.novelContainer.addEventListener("click", (e) => {
+      if (!e.target.classList.contains("delete-btn")) {
+        return;
+      }
+      console.log("delete part");
+    });
+    this.newNovelPartForm = document.querySelector(".new-novel-part-form");
   }
   showSpinner() {
     this.body.classList.add("logging");
@@ -17,6 +27,7 @@ class UiCtrl {
   }
   showLogin() {
     this.body.classList.remove("logged-in");
+    this.updateNovel([]);
     this.removeSpinner();
   }
   removeLogin() {
@@ -31,6 +42,15 @@ class UiCtrl {
   updateUserImage(image) {
     this.photoUser.setAttribute("src", image);
   }
+  updateNovel(novelParts) {
+    this.novelContainer.innerHTML = "";
+    novelParts.forEach((novelPart) => {
+      const div = document.createElement("div");
+      div.innerHTML = templateNovelPortion(novelPart);
+      this.novelContainer.appendChild(div);
+    });
+  }
+  onClickNovelPartDeleteBtn(_cb) {}
 }
 
 export default UiCtrl;
