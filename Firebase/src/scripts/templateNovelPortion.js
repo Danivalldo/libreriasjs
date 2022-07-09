c975567eb1a1afe71244167928201a9c5ab4029f
Dani Valldosera <dani@dimo.cat>
2022-07-09 14:07:11 +0200

    improved layout

--- Firebase/src/scripts/templateNovelPortion.js
+++ Firebase/src/scripts/templateNovelPortion.js
@@ -1,7 +1,15 @@
 const templateNovelPortion = (portion) => {
   return `
     <div id="${portion.id}" class="novel-part-container">
-      ${portion.isOwner ? `<button class="delete-btn">🗑️</button>` : ""}
+      ${
+        portion.isOwner && portion.canDelete
+          ? `<button class="delete-btn">
+              <span class="delete-label">
+                Si pasa 1h o más despúes de publicar, ya no podrás eliminarlo
+              </span>🗑️
+            </button>`
+          : ""
+      }
       <div>
         ${portion.data.pharagraph}
       </div>
