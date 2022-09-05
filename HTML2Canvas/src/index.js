4e9e492e981a5460fdaa87954b3a67c8f21d5237
Dani <dani@dimo.cat>
2022-09-05 15:19:51 +0200

    improved example

---
+++ HTML2Canvas/src/index.js
@@ -0,0 +1,15 @@
+import html2canvas from "html2canvas";
+import "./SCSS/index.scss";
+
+const captureBtn = document.querySelector("#capture-btn");
+
+captureBtn.addEventListener("click", async () => {
+  const canvas = await html2canvas(document.body, {
+    allowTaint: true,
+    // useCORS: true,
+  });
+  console.log(canvas);
+  document.body.appendChild(canvas);
+});
+
+console.log("Ready to go!", html2canvas);
