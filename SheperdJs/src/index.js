354bad23f68e5931c5800f1b9bba0ae027e6e8c0
Dani <dani@dimo.cat>
2022-09-28 15:34:55 +0200

    improved tour

--- SheperdJs/src/index.js
+++ SheperdJs/src/index.js
@@ -15,64 +15,107 @@ const tour = new Shepherd.Tour({
 tour.addSteps([
   {
     id: "step-1",
-    title: "Step 1",
-    text: "This step is attached to the bottom of the <code>.example-css-selector</code> element.",
+    title: "Menú de navegación",
+    text: "Este es el menú principal, navega entre apartados a través de sus iconos.",
     attachTo: {
       element: ".menu",
       on: "right",
     },
     buttons: [
       {
-        text: "Next",
+        text: "Siguiente",
         action: tour.next,
-      },
-      {
-        text: "More info",
-        action: () => {
-          alert("cosas!");
-        },
       },
     ],
   },
   {
     id: "step-2",
-    title: "Step 2",
-    text: "This is the second step",
+    title: "Tour virtual",
+    text: "Puedes repetir este tutorial en cualquier momento haciendo click en el botón de esta caja.",
     attachTo: {
       element: ".take-screenshot-card",
       on: "bottom",
     },
     buttons: [
       {
-        text: "Next",
-        action: tour.next,
+        text: "Anterior",
+        action: tour.back,
       },
       {
-        text: "Prev",
-        action: tour.back,
+        text: "Más información",
+        action: () => {
+          alert("Más información");
+        },
+      },
+      {
+        text: "Siguiente",
+        action: tour.next,
       },
     ],
   },
   {
-    id: "step-2",
-    text: "This is the second step",
+    id: "step-3",
+    title: "Gráfico de proyectos",
+    text: "Un primer vistazo del estado de tus proyectos",
     attachTo: {
-      element: ".footer-container",
-      on: "bottom",
+      element: ".card-circle-projects",
+      on: "auto",
     },
     buttons: [
       {
-        text: "Next",
-        action: tour.next,
+        text: "Anterior",
+        action: tour.back,
       },
       {
-        text: "Prev",
+        text: "Siguiente",
+        action: tour.next,
+      },
+    ],
+  },
+  {
+    id: "step-4",
+    title: "Gráfico de evolución",
+    text: "Evalúa como avanza el rendimiento",
+    attachTo: {
+      element: ".card-linecharts",
+      on: "auto",
+    },
+    buttons: [
+      {
+        text: "Anterior",
         action: tour.back,
+      },
+      {
+        text: "Siguiente",
+        action: tour.next,
+      },
+    ],
+  },
+  {
+    id: "step-5",
+    title: "Logout de la aplicación",
+    text: "Cierra la sessión a través de éste panel",
+    attachTo: {
+      element: ".user-badge",
+      on: "auto",
+    },
+    buttons: [
+      {
+        text: "Anterior",
+        action: tour.back,
+      },
+      {
+        text: "Finalizar",
+        action: tour.next,
       },
     ],
   },
 ]);
 
 window.addEventListener("load", () => {
+  tour.start();
+});
+
+document.querySelector(".tour-btn").addEventListener("click", () => {
   tour.start();
 });
