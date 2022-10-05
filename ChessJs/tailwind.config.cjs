adcd73a2ad344bc7d22e38101acfa593aa4dd7f1
Dani <dani@dimo.cat>
2022-10-05 23:17:12 +0200

    added plugin and content

--- ChessJs/tailwind.config.cjs
+++ ChessJs/tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html, js}"],
+  content: ["./**/*.{html, js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
