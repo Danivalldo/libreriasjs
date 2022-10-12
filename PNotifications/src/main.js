707a98dc4422b391694b556775e5818888c86955
Dani <dani@dimo.cat>
2022-10-12 12:48:33 +0200

    used onRecieveNotification method

--- PNotifications/src/main.js
+++ PNotifications/src/main.js
@@ -4,3 +4,7 @@ import FirebaseCtrl from "./services/FirebaseCtrl";
 const fireBaseCtrl = new FirebaseCtrl();
 
 fireBaseCtrl.initApp();
+fireBaseCtrl.onRecieveNotification((notification) => {
+  document.title = notification.title;
+  document.querySelector("h1").innerHTML = notification.body;
+});
