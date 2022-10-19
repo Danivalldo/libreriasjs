a5f6158e07309f26ce92d130907c81c1bfefbc99
dani_pc <dani@dimo.cat>
2022-10-19 20:32:51 +0200

    improved script

--- JsQR/main.js
+++ JsQR/main.js
@@ -1,8 +1,20 @@
 import "./style.sass";
 import qrcode from "qrcode";
+import {} from "jsqr";
 
-qrcode.toCanvas(document.querySelector("canvas"), "https://dimo.cat", (err) => {
-	if (err) {
-		return console.log(err);
-	}
+const input = document.querySelector("input");
+
+input.addEventListener("input", (e) => {
+	const text = e.target.value;
+	buildQR(text);
 });
+
+const buildQR = (text) => {
+	qrcode.toCanvas(document.querySelector("canvas"), text, (err) => {
+		if (err) {
+			return console.log(err);
+		}
+	});
+};
+
+buildQR("https://libreriasjs.com");
