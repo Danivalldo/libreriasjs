32c59817e0f3dfdbf840d1ee6012cd6d80ddedad
Dani Valldosera <dani@dimo.cat>
2022-10-23 20:06:44 +0200

    moved file to its own folder

--- PNotifications/local-api/local-push-notification.js
+++ PNotifications/local-api/local-push-notification.js
@@ -2,7 +2,7 @@ import CustomEnv from "custom-env";
 
 CustomEnv.env("local");
 
-import PushNotification from "./PushNotification.js";
+import PushNotification from "../api/PushNotification.js";
 
 //app token: fHmxjiYnT5aJN0va1jCJZC:APA91bEepoixm3_9Xr76hGvhN1Mrcvtt2JsiL7gTThR9VpZY2wppLulQJ8-yzblP8H8D5GyCgaIPrbZO-8WEQ94F2El0aooOZ53wARcPOgYSsnOtVo1g8Joij5Y-KR7Qg4VNXfwySJ2Q
 
@@ -12,7 +12,7 @@ import PushNotification from "./PushNotification.js";
 
 const pushNotification = new PushNotification();
 pushNotification.sendMessage(
-  "fFajlWh2ItYVxwvQ8oq8UB:APA91bGBPKCSxqDQPq0zK6jSAs7qjQzo646fIJTyS-fzYPwT3IhP9Txfp4Y50eM_K6DlH1UudgGiJPW1Ao06dwIfF3nT9Jf4P0Sd4dux96NuelhzSKAOJnmJFshXHuu-M7bDdnEbDz3_",
+  "e6YF2B_sq6ow8Nr6dHoGZ6:APA91bETsWJc0YJHzg0YrH7dxVTOK_l8xi4vKc9PIIXgZWd9x-VSX_INojw-WH0QkZvGksuF80bAf0DxQOEdnuFbafUhT018SOktILQA9Iw1dlK_F8i0wLZdi-hTGBfCNKc74ZFXNJUS",
   {
     notification: {
       title: "Nueva entrada!",
@@ -22,7 +22,7 @@ pushNotification.sendMessage(
       notification: {
         title: "Nueva entrada!",
         body: "Una nueva entrada del blog acaba de ser publicada",
-        click_action: "https://127.0.0.1:5173/",
+        click_action: "https://pn-otifications.vercel.app", //https://127.0.0.1:5173/
       },
     },
     data: {
