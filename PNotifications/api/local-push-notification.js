eebedc538dbc0f2aef39231ea1c27efd32e4a0ec
Dani <dani@dimo.cat>
2022-10-23 15:21:09 +0200

    added specific behaviour for webpush notification

--- PNotifications/api/local-push-notification.js
+++ PNotifications/api/local-push-notification.js
@@ -8,13 +8,22 @@ import PushNotification from "./PushNotification.js";
 
 //otro token fjPkF5W8Z8cElS_LBCx4XP:APA91bFdsOomlhfaDCekf1XuvDn4_Nhn1i9YPW-8lg9SYp6tO7G_tpoz7pgtydZZKPalqgQ_Zjw52XSLMNTlmHbPXLW7SqEs4NEvSfbYt7u1fwzweYQLMF3MHw84YNXyXQY_PucVGbGH
 
+//local web fFajlWh2ItYVxwvQ8oq8UB:APA91bGBPKCSxqDQPq0zK6jSAs7qjQzo646fIJTyS-fzYPwT3IhP9Txfp4Y50eM_K6DlH1UudgGiJPW1Ao06dwIfF3nT9Jf4P0Sd4dux96NuelhzSKAOJnmJFshXHuu-M7bDdnEbDz3_
+
 const pushNotification = new PushNotification();
 pushNotification.sendMessage(
-  "fHmxjiYnT5aJN0va1jCJZC:APA91bEepoixm3_9Xr76hGvhN1Mrcvtt2JsiL7gTThR9VpZY2wppLulQJ8-yzblP8H8D5GyCgaIPrbZO-8WEQ94F2El0aooOZ53wARcPOgYSsnOtVo1g8Joij5Y-KR7Qg4VNXfwySJ2Q",
+  "fFajlWh2ItYVxwvQ8oq8UB:APA91bGBPKCSxqDQPq0zK6jSAs7qjQzo646fIJTyS-fzYPwT3IhP9Txfp4Y50eM_K6DlH1UudgGiJPW1Ao06dwIfF3nT9Jf4P0Sd4dux96NuelhzSKAOJnmJFshXHuu-M7bDdnEbDz3_",
   {
     notification: {
       title: "Nueva entrada!",
       body: "Una nueva entrada del blog acaba de ser publicada",
+    },
+    webpush: {
+      notification: {
+        title: "Nueva entrada!",
+        body: "Una nueva entrada del blog acaba de ser publicada",
+        click_action: "https://127.0.0.1:5173/",
+      },
     },
     data: {
       title: "Japón - Dia 4",
