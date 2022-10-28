3cd09637c1d94953e96264d9a678a9e296883429
Dani <dani@dimo.cat>
2022-10-28 12:12:46 +0200

    edit server param depending on NODE_ENV

--- PNotifications/capacitor.config.ts
+++ PNotifications/capacitor.config.ts
@@ -1,10 +1,17 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.libreriasjs.pnotifications',
-  appName: 'PNotifications',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  appId: "com.libreriasjs.pnotifications",
+  appName: "PNotifications",
+  webDir: "dist",
+  bundledWebRuntime: false,
+  server:
+    process.env.NODE_ENV === "dev"
+      ? {
+          url: "http://192.168.1.135:5173",
+          cleartext: true,
+        }
+      : undefined,
 };
 
 export default config;
