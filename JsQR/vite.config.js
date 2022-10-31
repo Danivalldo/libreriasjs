63bb3a4a90d70692bc26e936e0773be3dc3e7ecd
Dani Valldosera <dani@dimo.cat>
2022-10-31 17:02:24 +0100

    added basicSSL config to vite

---
+++ JsQR/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+export default defineConfig({
+  server: {
+    https: false,
+  },
+  plugins: [basicSsl()],
+});
