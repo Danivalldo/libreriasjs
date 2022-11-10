1e4925f5f4f8ef98d50529df798b1bb355a280b2
Dani <dani@dimo.cat>
2022-11-10 12:47:03 +0100

    improved routing to allow static server and api requests

--- Express/index.js
+++ Express/index.js
@@ -1,18 +1,30 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import Express from "express";
+import express from "express";
 import helmet from "helmet";
+import path from "path";
 
-const app = Express();
+const app = express();
+const apiRouter = express.Router();
+const publicFolder = path.join(".", "dist");
 
 app.use(helmet());
 
-app.get("/", (req, res) => {
+app.use(express.static(publicFolder));
+// app.get("/", express.static(publicFolder));
+
+app.use("/api", apiRouter);
+
+apiRouter.get("/", (req, res) => {
   debugger;
   res.json({
     status: "ok",
   });
 });
+
+// app.use((req, res) => {
+//   res.status(404);
+// });
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on port ${process.env.PORT}`);
