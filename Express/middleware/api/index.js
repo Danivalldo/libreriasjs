f364a5ccdb59362b8bcf288870dfe192e3b004ea
Dani Valldosera <dani@dimo.cat>
2022-11-11 21:10:46 +0100

    prevent all kind of request under apiRouter for non authenticated users

--- Express/middleware/api/index.js
+++ Express/middleware/api/index.js
@@ -2,6 +2,13 @@ import express from "express";
 
 export const apiRouter = express.Router();
 
+apiRouter.use((req, res, next) => {
+  if (!req.isAuth) {
+    return next(new Error("User unathorized!"));
+  }
+  next();
+});
+
 apiRouter.get("/", (req, res) => {
   res.json({
     status: "ok",
