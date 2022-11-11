5a9c1e70eaf2c8657c3bafdb541be210fb841ad3
Dani Valldosera <dani@dimo.cat>
2022-11-11 17:35:41 +0100

    moved layout directly to html and setup login

--- Express/client/main.js
+++ Express/client/main.js
@@ -1,28 +1,6 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
 import { setupPerformRequest } from "./performRequest";
+import { setupLogin } from "./setupLogin";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <div class="card">
-      <button id="fetchBtn" type="button">Perform request</button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
 setupPerformRequest(document.querySelector("#fetchBtn"));
+setupLogin(document.querySelector("form"));
