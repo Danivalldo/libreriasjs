968320751fa1b5496e71c4378f42015f759bb02d
Dani Valldosera <dani@dimo.cat>
2022-11-11 21:10:15 +0100

    set header authorization with getToken

--- Express/client/performRequest.js
+++ Express/client/performRequest.js
@@ -1,7 +1,15 @@
+import { getToken } from "./setupLogin";
+
 export const setupPerformRequest = async (element) => {
   element.addEventListener("click", async () => {
     try {
-      const response = await fetch("./api");
+      const response = await fetch("./api", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: getToken(),
+        },
+      });
       const data = await response.json();
       console.log(data);
     } catch (err) {
