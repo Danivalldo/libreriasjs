1ebc2bece9d133caa3b3f9f9b911dea8c0556e6b
Dani <dani@dimo.cat>
2022-11-15 10:47:47 +0100

    added perform delete request

--- Express/client/performRequest.js
+++ Express/client/performRequest.js
@@ -10,6 +10,30 @@ export const setupPerformRequest = async (element) => {
           Authorization: getToken(),
         },
       });
+      if (response.status !== 200) {
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+export const setupPerformDelete = async (element) => {
+  element.addEventListener("click", async () => {
+    try {
+      const response = await fetch(`./api/2`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: getToken(),
+        },
+      });
+      if (response.status !== 200) {
+        throw new Error(response.statusText);
+      }
       const data = await response.json();
       console.log(data);
     } catch (err) {
