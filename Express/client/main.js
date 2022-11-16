2f93809b0ffb357772844d69d174a148c904f17c
Dani <dani@dimo.cat>
2022-11-16 16:45:54 +0100

    improved main script

--- Express/client/main.js
+++ Express/client/main.js
@@ -1,7 +1,25 @@
 import "./style.css";
 import { setupPerformRequest, setupPerformDelete } from "./performRequest";
 import { setupLogin } from "./setupLogin";
+import { setupMoviesUI } from "./setupMoviesUI";
 
-setupPerformRequest(document.querySelector("#fetchBtn"));
-setupPerformDelete(document.querySelector("#deleteMovieBtn"));
-setupLogin(document.querySelector("form"));
+const updateMovies = setupMoviesUI(document.querySelector("#moviesContainer"));
+
+const onGetMovies = (movies) => {
+  updateMovies(movies);
+};
+
+const onDeleteMovie = () => {};
+
+const onLoginSuccess = () => {};
+
+const onLogOutSuccess = () => {};
+
+setupPerformRequest(document.querySelector("#fetchBtn"), onGetMovies);
+setupPerformDelete(document.querySelector("#moviesContainer"), onDeleteMovie);
+
+setupLogin(
+  document.querySelector("form.login-form"),
+  onLoginSuccess,
+  onLogOutSuccess
+);
