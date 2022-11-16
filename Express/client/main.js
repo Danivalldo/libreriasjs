e98576aac40c20f66838d429f421397a01cb552f
Dani <dani@dimo.cat>
2022-11-16 22:35:26 +0100

    removed scripts and used only main script

--- Express/client/main.js
+++ Express/client/main.js
@@ -1,25 +1,46 @@
 import "./style.css";
-import { setupPerformRequest, setupPerformDelete } from "./performRequest";
-import { setupLogin } from "./setupLogin";
+import loginManager from "./services/LoginManager";
+import moviesManager from "./services/MoviesManager";
 import { setupMoviesUI } from "./setupMoviesUI";
 
-const updateMovies = setupMoviesUI(document.querySelector("#moviesContainer"));
+const formElement = document.querySelector("form.login-form");
+const getMoviesBtn = document.querySelector("#fetchBtn");
+const moviesContainer = document.querySelector("#moviesContainer");
 
-const onGetMovies = (movies) => {
-  updateMovies(movies);
-};
+const updateMovies = setupMoviesUI(moviesContainer);
 
-const onDeleteMovie = () => {};
+getMoviesBtn.addEventListener("click", async () => {
+  try {
+    const movies = await moviesManager.getMovies();
+    updateMovies(movies);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-const onLoginSuccess = () => {};
+moviesContainer.addEventListener("click", async (e) => {
+  if (!e.target.classList.contains("delete-movie-btn")) {
+    return;
+  }
+  const id = e.target.closest(".movie-card").id;
+  try {
+    await moviesManager.deleteMovie(id);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-const onLogOutSuccess = () => {};
-
-setupPerformRequest(document.querySelector("#fetchBtn"), onGetMovies);
-setupPerformDelete(document.querySelector("#moviesContainer"), onDeleteMovie);
-
-setupLogin(
-  document.querySelector("form.login-form"),
-  onLoginSuccess,
-  onLogOutSuccess
-);
+formElement.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const usernameInput = e.target.querySelector('input[name="username"]');
+  const passInput = e.target.querySelector('input[name="password"]');
+  const username = usernameInput.value;
+  const pass = passInput.value;
+  usernameInput.value = "";
+  passInput.value = "";
+  if (!username || !pass) {
+    return;
+  }
+  const token = await loginManager.login(username, pass);
+  console.log(token);
+});
