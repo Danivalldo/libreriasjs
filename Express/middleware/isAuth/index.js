9a0a62402d583fbda06e48777fcfdd0e0dc53bb0
dani_pc <dani@dimo.cat>
2022-11-18 17:45:02 +0100

    removed login and moved to its own

--- Express/middleware/isAuth/index.js
+++ Express/middleware/isAuth/index.js
@@ -1,6 +1,4 @@
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { getUserByUsername } from "../../services/users.js";
 
 export const isAuthMiddleware = async (req, res, next) => {
 	req.isAuth = false;
@@ -19,29 +17,4 @@ export const isAuthMiddleware = async (req, res, next) => {
 	}
 	req.isAuth = true;
 	next();
-};
-
-export const login = async (req, res, next) => {
-	const { username, pass } = req.body;
-
-	const user = getUserByUsername(username);
-
-	if (!user) {
-		return res.sendStatus(400);
-	}
-
-	bcrypt.compare(pass, user.pass, (err, result) => {
-		console.log(result, err);
-		if (err || !result) {
-			return res.sendStatus(400);
-		}
-
-		const token = jwt.sign({ username }, process.env.SECRET_TOKEN, {
-			expiresIn: "30min",
-		});
-
-		res.json({
-			token,
-		});
-	});
 };
