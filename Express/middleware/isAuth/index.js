43c593be900647d063de2d34e8daeb0d634a929f
dani_pc <dani@dimo.cat>
2022-11-18 17:20:35 +0100

    improved and formatted

--- Express/middleware/isAuth/index.js
+++ Express/middleware/isAuth/index.js
@@ -1,35 +1,47 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { getUserByUsername } from "../../services/users.js";
 
 export const isAuthMiddleware = async (req, res, next) => {
-  req.isAuth = false;
-  const token = req.get("Authorization");
-  if (!token) {
-    return next();
-  }
-  let decodedToken = undefined;
-  try {
-    decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-  } catch (err) {
-    return next();
-  }
-  if (!decodedToken) {
-    return next();
-  }
-  req.isAuth = true;
-  next();
+	req.isAuth = false;
+	const token = req.get("Authorization");
+	if (!token) {
+		return next();
+	}
+	let decodedToken = undefined;
+	try {
+		decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+	} catch (err) {
+		return next();
+	}
+	if (!decodedToken) {
+		return next();
+	}
+	req.isAuth = true;
+	next();
 };
 
 export const login = async (req, res, next) => {
-  const { username, pass } = req.body;
-  if (username !== "user" || pass !== "1234") {
-    return res.sendStatus(400);
-  }
+	const { username, pass } = req.body;
 
-  const token = jwt.sign({ username }, process.env.SECRET_TOKEN, {
-    expiresIn: "30min",
-  });
+	const user = getUserByUsername(username);
 
-  res.json({
-    token,
-  });
+	if (!user) {
+		return res.sendStatus(400);
+	}
+
+	bcrypt.compare(pass, user.pass, (err, result) => {
+		console.log(result, err);
+		if (err || !result) {
+			return res.sendStatus(400);
+		}
+
+		const token = jwt.sign({ username }, process.env.SECRET_TOKEN, {
+			expiresIn: "30min",
+		});
+
+		res.json({
+			token,
+		});
+	});
 };
