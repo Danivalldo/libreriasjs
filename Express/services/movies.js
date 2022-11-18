00fff533aea63ee18085cb54a7193bbd09b95e63
Dani <dani@dimo.cat>
2022-11-18 21:47:39 +0100

    added sanitizeHtml and schema validation

--- Express/services/movies.js
+++ Express/services/movies.js
@@ -1,45 +1,53 @@
 import JSONdb from "simple-json-db";
 import path from "path";
 import crypto from "crypto";
+import { string, number, object } from "yup";
+import sanitizeHtml from "sanitize-html";
 
 const db = new JSONdb(path.join(".", "db", "databaseMovies.json"));
+const schema = object({
+  id: string().uuid().required(),
+  name: string().required(),
+  score: number().min(1).max(5).required(),
+});
 
 export const getAllMovies = () => {
-	return db.get("movies");
+  return db.get("movies");
 };
 
-export const addMovie = (movie) => {
-	//input validation
-	const newMovie = {
-		id: crypto.randomUUID(),
-		name: typeof movie.name === "string" ? movie.name : "",
-		score: typeof movie.score === "number" ? movie.score : 1,
-	};
-	const movies = db.get("movies");
-	movies.push(newMovie);
-	db.set("movies", movies);
+export const addMovie = async (movie) => {
+  //input validation
+  const newMovie = {
+    ...movie,
+    id: crypto.randomUUID(),
+    name: sanitizeHtml(movie.name, { allowedTags: [] }),
+  };
+  await schema.validate(newMovie);
+  const movies = db.get("movies");
+  movies.push(newMovie);
+  db.set("movies", movies);
 };
 
 export const deleteMovie = (id) => {
-	const movies = db.get("movies");
-	db.set(
-		"movies",
-		movies.filter((movie) => movie.id !== id)
-	);
+  const movies = db.get("movies");
+  db.set(
+    "movies",
+    movies.filter((movie) => movie.id !== id)
+  );
 };
 
 export const updateMovie = (id, newContent) => {
-	const movies = db.get("movies");
-	db.set(
-		"movies",
-		movies.map((movie) => {
-			if (movie.id !== id) {
-				return movie;
-			}
-			return {
-				...movie,
-				...newContent,
-			};
-		})
-	);
+  const movies = db.get("movies");
+  db.set(
+    "movies",
+    movies.map((movie) => {
+      if (movie.id !== id) {
+        return movie;
+      }
+      return {
+        ...movie,
+        ...newContent,
+      };
+    })
+  );
 };
