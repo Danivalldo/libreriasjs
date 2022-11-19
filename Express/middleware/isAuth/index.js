cacb67e3ecaeda0429597193d04914e0c86b9338
Dani <dani@dimo.cat>
2022-11-19 01:40:10 +0100

    refactored isAuthMiddleware

--- Express/middleware/isAuth/index.js
+++ Express/middleware/isAuth/index.js
@@ -1,20 +1,15 @@
 import jwt from "jsonwebtoken";
 
 export const isAuthMiddleware = async (req, res, next) => {
-	req.isAuth = false;
-	const token = req.get("Authorization");
-	if (!token) {
-		return next();
-	}
-	let decodedToken = undefined;
-	try {
-		decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-	} catch (err) {
-		return next();
-	}
-	if (!decodedToken) {
-		return next();
-	}
-	req.isAuth = true;
-	next();
+  req.isAuth = false;
+  const token = req.get("Authorization");
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+    if (decodedToken) {
+      req.isAuth = true;
+    }
+  } catch (err) {
+  } finally {
+    next();
+  }
 };
