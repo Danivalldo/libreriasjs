379db32f3512ed82047d0dd7122391a5f0a4e070
dani_pc <dani@dimo.cat>
2022-11-19 16:39:20 +0100

    formatted and improved

--- Express/client/services/MoviesManager.js
+++ Express/client/services/MoviesManager.js
@@ -1,42 +1,43 @@
 import loginManager from "./LoginManager";
 
 class MoviesManager {
-  constructor() {
-    this.endpoint = "./api";
-  }
+	constructor() {
+		this.endpoint = "./api";
+	}
 
-  async performRequest(method = "GET", endpoint = "", body) {
-    const response = await fetch(`${this.endpoint}${endpoint}`, {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: loginManager.getToken(),
-      },
-      body,
-    });
-    if (response.status !== 200) {
-      throw new Error(response.statusText);
-    }
-    return response;
-  }
+	async performRequest(method = "GET", endpoint = "", body) {
+		const response = await fetch(`${this.endpoint}${endpoint}`, {
+			method,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: loginManager.getToken(),
+			},
+			body,
+		});
+		const data = await response.json();
+		if (data.error) {
+			throw new Error(data.error);
+		}
+		return data;
+	}
 
-  async getMovies() {
-    const response = await this.performRequest("GET", "/");
-    const data = await response.json();
-    return data;
-  }
+	async getMovies() {
+		const data = await this.performRequest("GET", "/");
 
-  async deleteMovie(id) {
-    await this.performRequest("DELETE", `/${id}`);
-  }
+		return data;
+	}
 
-  async addMovie(movie) {
-    await this.performRequest("POST", "/", JSON.stringify(movie));
-  }
+	async deleteMovie(id) {
+		await this.performRequest("DELETE", `/${id}`);
+	}
 
-  async updateMovie(id, movie) {
-    await this.performRequest("PUT", `/${id}`, JSON.stringify(movie));
-  }
+	async addMovie(movie) {
+		await this.performRequest("POST", "/", JSON.stringify(movie));
+	}
+
+	async updateMovie(id, movie) {
+		await this.performRequest("PUT", `/${id}`, JSON.stringify(movie));
+	}
 }
 
 const moviesManager = new MoviesManager();
