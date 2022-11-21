abbd969146bd1c402f52882faab553913aaa15b2
Dani <dani@dimo.cat>
2022-11-21 10:39:00 +0100

    added prop createdBy and filter each action to ensure that only the creator can manage its own movies

--- Express/services/movies.js
+++ Express/services/movies.js
@@ -9,20 +9,23 @@ const schemaNewMovie = object({
   id: string().uuid().required(),
   name: string().required(),
   score: number().min(1).max(5).required(),
+  createdBy: string().email().required(),
 })
   .noUnknown(true)
   .required()
   .strict();
 
-export const getAllMovies = () => {
-  return db.get("movies");
+export const getAllMovies = (username) => {
+  const movies = db.get("movies");
+  return movies.filter((movie) => movie.createdBy === username);
 };
 
-export const addMovie = async (movie) => {
+export const addMovie = async (movie, username) => {
   const newMovie = {
     ...movie,
     id: crypto.randomUUID(),
     name: sanitizeHtml(movie.name, { allowedTags: [] }),
+    createdBy: username,
   };
   await schemaNewMovie.validate(newMovie);
   const movies = db.get("movies");
@@ -31,21 +34,27 @@ export const addMovie = async (movie) => {
   return;
 };
 
-export const deleteMovie = (id) => {
+export const deleteMovie = (id, username) => {
   const movies = db.get("movies");
   const indexMovie = movies.findIndex((movie) => movie.id === id);
   if (indexMovie < 0) {
     throw new Error("This movie does not exists");
   }
+  if (movies[indexMovie].createdBy !== username) {
+    throw new Error("You are not the creator");
+  }
   movies.splice(indexMovie, 1);
   db.set("movies", movies);
 };
 
-export const updateMovie = async (id, newContent) => {
+export const updateMovie = async (id, newContent, username) => {
   const movies = db.get("movies");
   const indexMovie = movies.findIndex((movie) => movie.id === id);
   if (indexMovie < 0) {
     throw new Error("This movie does not exists");
+  }
+  if (movies[indexMovie].createdBy !== username) {
+    throw new Error("You are not the creator");
   }
   const updatedMovie = {
     ...movies[indexMovie],
