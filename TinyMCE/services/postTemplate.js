ead07912ddbecbf37d47376be999fe1cfdce3e98
Dani <dani@dimo.cat>
2022-11-28 23:10:07 +0100

    improved template

--- TinyMCE/services/postTemplate.js
+++ TinyMCE/services/postTemplate.js
@@ -1,6 +1,19 @@
 const postTemplate = (body) => {
   const template = `
-      ${body}
+    <div class="post-header">
+      <span class="snap-container">
+        <img
+          src="https://randomuser.me/api/portraits/${
+            Math.random() > 0.5 ? "men" : "women"
+          }/${Math.floor(Math.random() * (100 - 1 + 1) + 1)}.jpg"
+          alt=""
+        />
+      </span>
+      <span class="username"><strong>@userexample</strong> dijo:</span>
+    </div>
+    <div class="post-body">
+    ${body}
+    </div>
   `;
   const divPost = document.createElement("div");
   divPost.classList.add("post-container");
