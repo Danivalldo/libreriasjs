5c496b581bdd38395a3277f090ff0c6358aa4c87
Dani <dani@dimo.cat>
2023-01-19 00:02:46 +0100

    added model-viewer with suzanne

--- RevealJs/main.js
+++ RevealJs/main.js
@@ -2,6 +2,7 @@ import "./style.sass";
 import Reveal from "reveal.js";
 // import Markdown from "reveal.js/plugin/markdown/markdown.esm";
 import Notes from "reveal.js/plugin/notes/notes.esm";
+import "@google/model-viewer";
 
 const deck = new Reveal({
   plugins: [Notes],
