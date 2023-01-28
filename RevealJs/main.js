27dcd5caadd12cea8bba672dfa54171dbc6f4d9c
Dani <dani@dimo.cat>
2023-01-29 00:02:49 +0100

    improved script

--- RevealJs/main.js
+++ RevealJs/main.js
@@ -1,24 +1,44 @@
 import "./style.sass";
 import Reveal from "reveal.js";
 import lottie from "lottie-web";
-// import Markdown from "reveal.js/plugin/markdown/markdown.esm";
+import Particles from "particlesjs";
 import Notes from "reveal.js/plugin/notes/notes.esm";
 import "@google/model-viewer";
+
+let particles = undefined;
 
 const deck = new Reveal({
   plugins: [Notes],
   hash: true,
   respondToHashChanges: true,
   history: true,
-  // controlsTutorial: true,
 });
 
 lottie.loadAnimation({
-  container: document.querySelector(".lottie-container"), // the dom element that will contain the animation
+  container: document.querySelector(".lottie-container"),
   renderer: "svg",
   loop: true,
   autoplay: true,
-  path: "lottie-animations/135022-jellyfish.json", // the path to the animation json
+  path: "lottie-animations/135022-jellyfish.json",
+});
+
+deck.on("slidechanged", (event) => {
+  if (event.indexh === 4 && event.indexv === 1) {
+    if (!particles) {
+      particles = Particles.init({
+        selector: ".background-particles",
+        connectParticles: true,
+        sizeVariations: 5,
+        color: "#1c4f3e",
+      });
+    } else {
+      particles.resumeAnimation();
+    }
+  } else {
+    if (particles) {
+      particles.pauseAnimation();
+    }
+  }
 });
 
 deck.initialize();
