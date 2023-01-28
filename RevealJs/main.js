ed4ac9361b0b818673c5587f0e3d2c61daeaf777
Dani <dani@dimo.cat>
2023-01-28 21:29:40 +0100

    launch lottie

--- RevealJs/main.js
+++ RevealJs/main.js
@@ -1,5 +1,6 @@
 import "./style.sass";
 import Reveal from "reveal.js";
+import lottie from "lottie-web";
 // import Markdown from "reveal.js/plugin/markdown/markdown.esm";
 import Notes from "reveal.js/plugin/notes/notes.esm";
 import "@google/model-viewer";
@@ -10,6 +11,14 @@ const deck = new Reveal({
   respondToHashChanges: true,
   history: true,
   // controlsTutorial: true,
+});
+
+lottie.loadAnimation({
+  container: document.querySelector(".lottie-container"), // the dom element that will contain the animation
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "lottie-animations/135022-jellyfish.json", // the path to the animation json
 });
 
 deck.initialize();
