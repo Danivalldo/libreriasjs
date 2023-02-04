b0b3cc64f6031fcc1a91a7fefa943dac0291731f
Dani <dani@dimo.cat>
2023-02-04 18:52:16 +0100

    changed lottie-web for lottie-player

--- RevealJs/main.js
+++ RevealJs/main.js
@@ -1,6 +1,6 @@
 import "./style.sass";
 import Reveal from "reveal.js";
-import lottie from "lottie-web";
+import "@lottiefiles/lottie-player";
 import Particles from "particlesjs";
 import Notes from "reveal.js/plugin/notes/notes.esm";
 import "@google/model-viewer";
@@ -12,14 +12,6 @@ const deck = new Reveal({
   hash: true,
   respondToHashChanges: true,
   history: true,
-});
-
-lottie.loadAnimation({
-  container: document.querySelector(".lottie-container"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "lottie-animations/135022-jellyfish.json",
 });
 
 deck.on("slidechanged", (event) => {
