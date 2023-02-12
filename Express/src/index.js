7a9db4210577765ceebb0297d8175bc2c2e5a16f
Dani <dani@dimo.cat>
2023-02-12 12:47:12 +0100

    created simple hello world express server

--- Express/src/index.js
+++ Express/src/index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+const PORT = 6006;
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("<h1>Hello world!</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on: http://localhost:${PORT}/`);
+});
