012ede2edc6e148bfe18995ff04905b9201bd507
Dani <dani@dimo.cat>
2023-02-16 00:16:28 +0100

    added timestamps

--- Express/src/middleware/api/index.js
+++ Express/src/middleware/api/index.js
@@ -5,11 +5,13 @@ export const apiRouter = Router();
 apiRouter.get("/", (req, res, next) => {
   res.status(200).json({
     status: "ok",
+    timestamp: Date.now(),
   });
 });
 
 apiRouter.post("/", (req, res) => {
   res.status(200).json({
+    timestamp: Date.now(),
     ...req.body,
   });
 });
