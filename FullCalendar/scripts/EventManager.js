16aca5582285bdb617f5e3a42ceea571370a94f7
Dani Valldosera <dani@dimo.cat>
2023-03-01 10:50:23 +0100

    improved logic

--- FullCalendar/scripts/EventManager.js
+++ FullCalendar/scripts/EventManager.js
@@ -1,6 +1,6 @@
 class EventManager {
   constructor() {
-    this.eventsKey = 'ljs-events';
+    this.eventsKey = "ljs-events";
     const events = localStorage.getItem(this.eventsKey);
     if (!events) {
       localStorage.setItem(this.eventsKey, JSON.stringify([]));
@@ -13,11 +13,25 @@ class EventManager {
     const events = this.getEvents();
     localStorage.setItem(this.eventsKey, JSON.stringify([event, ...events]));
   }
+
+  updateEvent(newEvent, id) {
+    const events = this.getEvents();
+    localStorage.setItem(
+      this.eventsKey,
+      JSON.stringify(
+        events.map((event) =>
+          event.id === id ? { ...event, ...newEvent } : event
+        )
+      )
+    );
+  }
+
   deleteEvent(id) {
     const events = this.getEvents();
-    localStorage.setItem(this.eventsKey, JSON.stringify(
-      events.filter((event) => event.id !== id)
-    ));
+    localStorage.setItem(
+      this.eventsKey,
+      JSON.stringify(events.filter((event) => event.id !== id))
+    );
   }
 }
 
