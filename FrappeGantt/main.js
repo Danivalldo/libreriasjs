bc31cd9bdea287069ec2322963c593a8c005b8b5
Dani <dani@dimo.cat>
2023-04-29 23:08:37 +0200

    Improved Gantt toolbar with filters and selectors

--- FrappeGantt/main.js
+++ FrappeGantt/main.js
@@ -5,20 +5,27 @@ import '@shoelace-style/shoelace/dist/components/split-panel/split-panel.js';
 import '@shoelace-style/shoelace/dist/components/range/range.js';
 import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
 import '@shoelace-style/shoelace/dist/components/checkbox/checkbox.js';
-// import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-
-// Set the base path to the folder you copied Shoelace's assets to
-// setBasePath('/path/to/shoelace/dist');
-
+import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
+import '@shoelace-style/shoelace/dist/components/select/select.js';
+import '@shoelace-style/shoelace/dist/components/option/option.js';
+import Litepicker from 'litepicker';
+import 'litepicker/dist/plugins/mobilefriendly';
+import dayjs from "dayjs";
 import GanttService from "./src/GanttService";
+
 import "./style.css";
 
 const colWidthInput = document.querySelector("#colWidthInput");
 const rowHeightInput = document.querySelector("#rowHeightInput");
 const hideLabelsInput = document.querySelector("#hideLabelsInput");
 const hideArrowsInput = document.querySelector("#hideArrowsInput");
+const createTaskBtn = document.querySelector('#create-task-btn');
+const dialog = document.querySelector('#dialog');
+const keyFilterInput = document.querySelector('#keyFilterInput');
+const viewModeSelect = document.querySelector('#select-view-mode');
 
-const ganttSrv = new GanttService("#gantt-container", [
+
+const tasks = [
   {
     id: "Task 1",
     name: "Task 1",
@@ -39,17 +46,45 @@ const ganttSrv = new GanttService("#gantt-container", [
     custom_class: "hidden",
     type: "workOrder",
   },
-]);
+];
+
+const ganttSrv = new GanttService("#gantt-container", tasks);
+
+ganttSrv.on('clicktask', () => {
+  dialog.show();
+})
+
+const picker = new Litepicker({
+  plugins: ['mobilefriendly'],
+  element: document.getElementById('date-filters-input'),
+  resetButton: true,
+  singleMode: false,
+});
+
+picker.on('selected', (date1, date2) => {
+  const dStartFilter = dayjs(date1.dateInstance);
+  const dEndFilter = dayjs(date2.dateInstance);
+  const filteredTasks = tasks.filter((task) => {
+    const dStartTask = dayjs(task.start);
+    const dEndTask = dayjs(task.end);
+    return dStartFilter.isBefore(dEndTask) && dEndFilter.isAfter(dStartTask);
+  });
+  ganttSrv.updateTasks(filteredTasks);
+});
+
+picker.on('clear:selection', () => {
+  ganttSrv.updateTasks([...tasks]);
+});
 
 window.addEventListener('load', () => {
   ganttSrv.updateGantt();
-})
+});
 
-colWidthInput.addEventListener("input", (e) => {
+colWidthInput.addEventListener("sl-input", (e) => {
   ganttSrv.updateColumnWidth(Number(e.currentTarget.value));
 });
 
-rowHeightInput.addEventListener("input", (e) => {
+rowHeightInput.addEventListener("sl-input", (e) => {
   ganttSrv.updateHeight(Number(e.currentTarget.value));
 });
 
@@ -60,3 +95,17 @@ hideLabelsInput.addEventListener("sl-change", (e) => {
 hideArrowsInput.addEventListener("sl-change", (e) => {
   ganttSrv.toogleArrows();
 });
+
+createTaskBtn.addEventListener('click', () => {
+  dialog.show();
+});
+
+keyFilterInput.addEventListener('sl-input', (e) => {
+  const filterKey = e.target.value;
+  ganttSrv.updateTasks(tasks.filter(task => task.name.includes(filterKey)))
+});
+
+viewModeSelect.addEventListener('sl-change', (e) => {
+  const viewMode = e.target.value;
+  ganttSrv.changeView(viewMode);
+})
--- FrappeGantt/vite.config.js
+++ FrappeGantt/vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
+  plugins: [
+    { src: '~/plugins/litepicker.js', mode: 'client' }
+  ],
 });
