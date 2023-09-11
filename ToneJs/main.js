e7ec50f6a90ae8c2d7ab569eb68bf919d07a4057
Dani Valldosera <dani@dimo.cat>
2023-09-11 21:34:31 +0200

    improve exercise with gird to create music

--- ToneJs/main.js
+++ ToneJs/main.js
@@ -1,2 +1,117 @@
 import "./style.css";
 import * as Tone from "tone";
+import { customSynth, piano, polySynth, clapSynth } from "./src/synths";
+import StepSequencer from "./src/StepSequencer";
+
+const { Transport, start, Destination } = Tone;
+
+const playNoteBtn = document.querySelector("#play-note-btn");
+const playSongBtn = document.querySelector("#play-song-btn");
+const stopSongBtn = document.querySelector("#stop-song-btn");
+const clapBtn = document.querySelector("#clap-btn");
+const intervalSlider = document.querySelector("#interval");
+const tempoSlider = document.querySelector("#tempo");
+const synthSelector = document.querySelector("#synth");
+
+// Create a musical sequence
+// const melody = [
+//   { note: "C4", duration: "4n" },
+//   { note: "D4", duration: "4n" },
+//   { note: "E4", duration: "4n" },
+//   { note: "C4", duration: "4n" },
+//   { note: "F4", duration: "4n" },
+//   { note: "E4", duration: "4n" },
+//   { note: "D4", duration: "4n" },
+//   { note: "G4", duration: "4n" },
+// ];
+
+const stepSequencer = new StepSequencer("#step-sequencer-container");
+
+// Play the melody
+let index = 0;
+let tempo = 120;
+let interval = "4n";
+let synth = polySynth;
+
+const playNote = () => {
+  clapSynth.triggerAttackRelease();
+  const notes = stepSequencer.getNotesByStep(index);
+
+  for (let i = 0, j = notes.length; i < j; i++) {
+    const note = notes[i];
+    synth.triggerAttackRelease(`${note}`, "4n");
+  }
+  // const { note, duration } = melody[index];
+
+  // synth.triggerAttackRelease(note, duration);
+
+  index = (index + 1) % stepSequencer.tracks;
+};
+
+const playSong = async () => {
+  if (Transport.state === "started") return;
+  // await start();
+  await Transport.start();
+  // Schedule the melody to play
+  Transport.scheduleRepeat((time) => {
+    playNote();
+  }, interval);
+
+  // Start the transport (metronome)
+  Transport.bpm.value = tempo; // Set the tempo
+  await start();
+};
+
+const stopSong = () => {
+  Transport.cancel();
+  Transport.stop();
+  index = 0;
+};
+
+const playCustomNote = async () => {
+  const now = Tone.now();
+  synth.triggerAttackRelease("C4", "5n", now);
+};
+
+const playClap = async () => {
+  clapSynth.triggerAttackRelease();
+};
+
+const onChangeSilderInterval = (e) => {
+  // console.log();
+  stopSong();
+  interval = `${e.target.value}n`;
+  playSong();
+};
+
+const onChangeSilderTempo = (e) => {
+  tempo = e.target.value;
+  Transport.bpm.value = tempo;
+};
+
+const onSelectSynth = (e) => {
+  switch (e.target.value) {
+    case "poly":
+      synth = polySynth;
+      return;
+    case "piano":
+      synth = piano;
+      return;
+    case "custom":
+    default:
+      synth = customSynth;
+      return;
+  }
+};
+
+playSongBtn.addEventListener("click", playSong);
+playNoteBtn.addEventListener("click", playCustomNote);
+stopSongBtn.addEventListener("click", stopSong);
+intervalSlider.addEventListener("change", onChangeSilderInterval);
+tempoSlider.addEventListener("change", onChangeSilderTempo);
+synthSelector.addEventListener("change", onSelectSynth);
+clapBtn.addEventListener("click", playClap);
+
+window.Transport = Transport;
+window.synth = synth;
+window.Destination = Destination;
---
+++ ToneJs/src/StepSequencer.js
@@ -0,0 +1,92 @@
+class StepSequencer {
+  constructor(containerSelector) {
+    this.container = document.querySelector(containerSelector);
+    this.toolbar = document.createElement("div");
+    this.toolbar.classList.add("step-sequencer__toolbar");
+    this.mainContainer = document.createElement("div");
+    this.mainContainer.classList.add("step-sequencer__main-container");
+    this.container.appendChild(this.toolbar);
+    this.container.appendChild(this.mainContainer);
+    this.tracks = 8;
+    this.selectedNotes = Array.from({ length: this.tracks }, () => []);
+    // this.selectedNotes = [
+    //   ["C"],
+    //   ["D"],
+    //   ["E"],
+    //   ["C"],
+    //   ["F"],
+    //   ["E"],
+    //   ["D"],
+    //   ["G"],
+    // ];
+    this.createGrid();
+    this.addListeners();
+  }
+  createGrid() {
+    // const notes = [
+    //   "C",
+    //   // "C#",
+    //   "D",
+    //   // "D#",
+    //   "E",
+    //   // "E#",
+    //   "F",
+    //   // "F#",
+    //   "G",
+    //   // "G#",
+    //   "A",
+    //   // "A#",
+    //   "B",
+    //   // "B#",
+    // ];
+
+    const notes = ["F4", "Eb4", "C4", "Bb3", "Ab3", "F3"];
+
+    for (let i = 0, j = notes.length; i < j; i++) {
+      const note = notes[i];
+      const row = document.createElement("div");
+      row.classList.add("notes-row");
+      // this.selectedNotes.push([]);
+      for (let n = 0; n < this.tracks; n++) {
+        const noteBtn = document.createElement("button");
+        noteBtn.setAttribute("data-note-name", note);
+        noteBtn.setAttribute("data-note-position", n);
+        noteBtn.innerHTML = note;
+        row.appendChild(noteBtn);
+        this.mainContainer.appendChild(row);
+      }
+    }
+  }
+  addListeners() {
+    this.mainContainer.addEventListener(
+      "pointerdown",
+      this.handleOnPressNote.bind(this)
+    );
+  }
+  handleOnPressNote(e) {
+    e.preventDefault();
+    const button = e.target;
+    const noteName = button.dataset.noteName;
+    const notePosition = button.dataset.notePosition;
+    if (!noteName) return;
+    const noteIndex = this.selectedNotes[notePosition].findIndex(
+      (note) => note === noteName
+    );
+
+    if (noteIndex >= 0) {
+      button.classList.remove("active");
+      return this.selectedNotes[notePosition].splice(noteIndex, 1);
+    }
+    button.classList.add("active");
+    this.selectedNotes[notePosition].push(noteName);
+  }
+  getSelectedNotes() {
+    return this.selectedNotes;
+  }
+  getNotesByStep(step) {
+    if (step > this.tracks - 1 || step < 0) return;
+    return this.selectedNotes[step];
+  }
+}
+
+export default StepSequencer;
