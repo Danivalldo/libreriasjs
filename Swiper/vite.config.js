cbe6dacd85cd87d569db29717055690eb86bc0b5
Dani Valldosera <dani@dimo.cat>
2023-09-25 18:02:09 +0200

    only one html

--- Swiper/vite.config.js
+++ Swiper/vite.config.js
@@ -3,12 +3,12 @@ import { resolve } from "path";
 
 export default defineConfig({
   base: "./",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        swiperWebComponent: resolve(__dirname, "swiper-web-component.html"),
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, "index.html"),
+  //       swiperWebComponent: resolve(__dirname, "swiper-web-component.html"),
+  //     },
+  //   },
+  // },
 });
