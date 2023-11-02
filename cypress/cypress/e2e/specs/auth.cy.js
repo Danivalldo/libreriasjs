b4feadf5bcecde35e8b06386c525c19756b9e469
Dani Valldosera <dani@dimo.cat>
2023-11-02 11:05:55 +0100

    add test to check redirection after session expired

--- cypress/cypress/e2e/specs/auth.cy.js
+++ cypress/cypress/e2e/specs/auth.cy.js
@@ -13,4 +13,19 @@ describe("Auth", () => {
     cy.location("pathname").should("eq", "/login");
     cy.getAllLocalStorage().should("be.empty");
   });
+  it("should redirect after session expired", () => {
+    cy.signIn();
+    cy.location("pathname").should("eq", "/");
+    //Sessions expired after 30min so we need to move time and stub backend response
+    cy.clock();
+    cy.tick(1000 * 60 * 60 * 1.5);
+    cy.intercept("/api/*", {
+      statusCode: 401,
+      body: { error: "Unauthorized" },
+    }).as("apiRequest");
+    cy.get('[data-cy-movie-id="Star Wars"] [data-cy="star-btn-2"]').click();
+    cy.wait("@apiRequest");
+    cy.location("pathname").should("eq", "/login");
+    cy.contains("Your session expired");
+  });
 });
