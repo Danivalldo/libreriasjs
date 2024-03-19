1401337d1a502cf6cb8ecb9e2f7d65ec9f3c1080
Dani Valldosera <dani@dimo.cat>
2024-03-19 22:42:37 +0100

    remove unnecesary data

--- Echarts/scripts/pieChart.js
+++ Echarts/scripts/pieChart.js
@@ -35,13 +35,7 @@ pieChart.setOption({
       labelLine: {
         show: false,
       },
-      data: [
-        { value: 1048, name: "Search Engine" },
-        { value: 735, name: "Direct" },
-        { value: 580, name: "Email" },
-        { value: 484, name: "Union Ads" },
-        { value: 300, name: "Video Ads" },
-      ],
+      data: [],
     },
   ],
 });
